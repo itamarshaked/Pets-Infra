038da3469524ed6064f7a70c3c0f593b54445b05
Itamar Shaked <Itamar@shaked.in>
2026-06-01 00:28:01 +0300

    Add VPC and public subnet

--- main.tf
+++ main.tf
@@ -7,3 +7,23 @@ resource "aws_s3_bucket" "terraform_state" {
     ManagedBy = "Terraform"
   }
 }
+
+resource "aws_vpc" "pets_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "pets-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.pets_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "pets-public-subnet"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,10 @@
 output "bucket_name" {
   value = aws_s3_bucket.terraform_state.bucket
 }
+output "vpc_id" {
+  value = aws_vpc.pets_vpc.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.public_subnet.id
+}

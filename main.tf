347489dc77f4b1311103c12b8cb66a9dddcfcca2
Itamar Shaked <Itamar@shaked.in>
2026-06-07 00:05:19 +0300

    Add RDS PostgreSQL instance

--- main.tf
+++ main.tf
@@ -215,3 +215,28 @@ resource "aws_security_group" "rds_sg" {
     Name = "pets-rds-sg"
   }
 }
+
+resource "aws_db_instance" "pets_db" {
+  identifier = "pets-db"
+
+  engine         = "postgres"
+  engine_version = "16"
+  instance_class = "db.t3.micro"
+
+  allocated_storage = 20
+  storage_type      = "gp2"
+
+  db_name  = "petsdb"
+  username = "petuser"
+  password = "petpass123"
+
+  db_subnet_group_name   = aws_db_subnet_group.pets_db_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  publicly_accessible = false
+  skip_final_snapshot = true
+
+  tags = {
+    Name = "pets-rds-postgres"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -28,3 +28,11 @@ output "private_subnet_2_id" {
 output "db_subnet_group_name" {
   value = aws_db_subnet_group.pets_db_subnet_group.name
 }
+
+output "rds_security_group_id" {
+  value = aws_security_group.rds_sg.id
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.pets_db.endpoint
+}

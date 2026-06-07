a3f5faecbfb572f6ded97a31be994ca0146fb669
Itamar Shaked <Itamar@shaked.in>
2026-06-07 22:29:05 +0300

    Load database credentials from Secrets Manager 3

--- main.tf
+++ main.tf
@@ -104,6 +104,8 @@ resource "aws_instance" "pets_server" {
   instance_type = "t3.micro"
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
+  user_data_replace_on_change = true
+
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.pets_sg.id]
 
